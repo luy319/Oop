56d5dad4dd4aa083fe4390d7d7d15ca90c7d22e9
luy319 <luyandadelight@gmail.com>
2025-09-19 12:34:06 +0000

    FootballPlayer

---
+++ Average.java
@@ -0,0 +1,20 @@
+import.util.Scanner
+public class Average{
+    public static void main(String []args){
+        Scanner myObj= new Scanner(System.in);
+        System.out.println("Enter first number");
+        int firstnumber=myObj.nextline();
+
+        System.out.println("Enter second number");
+        int secondnumber=myObj.nextline();
+
+        System.out.println("Enter third number");
+        int thirdnumber=myObj.nextline();
+
+        System.out.println("Enter lastnumber");
+        int lastnumber=myObj.nextline();
+
+        float average=(firstnumber+secondnumber+thirdnumber+lastnumber;)
+        System.out.println(average)
+    }
+}
